f76715ce13c85e7823655e3b3c60ee0eecb45fd2
Jack Quimby <jack.quimby54@gmail.com>
2023-10-27 18:07:06 -0400

    lots of changes

--- true_odds/models.py
+++ true_odds/models.py
@@ -1,11 +1,19 @@
 from . import db
 
 class Game(db.Model):
-    index = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date = db.Column(db.String(20))
-    A = db.Column(db.String(50))  # Adjust the type and length accordingly
-    B = db.Column(db.String(50))
+    A = db.Column(db.String(50), db.ForeignKey('team.team_name'))  # Adjust the type and length accordingly
+    B = db.Column(db.String(50), db.ForeignKey('team.team_name'))
     A_win = db.Column(db.Float)
     B_win = db.Column(db.Float)
     ABS_diff = db.Column(db.Float)
     prediction = db.Column(db.String(50))
+    #teams = db.Column(db.Integer, db.ForeignKey('team.team_name'))
+
+class Team(db.Model):
+    team_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    team_name = db.Column(db.String(20))
+    games = db.relationship('Game', backref='team_name', lazy=True)
+
+    
--- true_odds/routes.py
+++ true_odds/routes.py
@@ -1,15 +1,24 @@
 from . import app, db
 import pandas as pd
+import sqlite3
 from flask import redirect, render_template, url_for
-from true_odds.models import Game
+from true_odds.models import Game, Team 
 from sqlalchemy import and_, or_
 from datetime import datetime
 today_date = datetime.now().date()
 
+paths = {
+        'game': 'all_season_preds',
+        'team': '2023_stats'
+         }
+
 def load_data():
-    csv_path = './true_odds/static/Database/all_season_preds.csv'
-    data = pd.read_csv(csv_path)
-    data.to_sql(name='game', con=db.engine, if_exists='replace', index=True)
+    conn = sqlite3.connect('./instance/predict.db')
+    for db_name, path in paths.items():
+        print(path)
+        csv_path = f'./true_odds/static/Database/{path}.csv'
+        data = pd.read_csv(csv_path)
+        data.to_sql(name=db_name, con=conn, if_exists='replace', index=True)
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home')
@@ -30,6 +39,11 @@ def team_page(teamname):
 def date_page(date):
     date_db = Game.query.filter(Game.date == date)
     return render_template('team_preds.html', name=date, table=date_db)
+
+@app.route('/stats/<teamname>')
+def team_stats_page(teamname):
+    team_db = Team.query.filter(Team.team_name == teamname)
+    return render_template('team_bio.html', name=teamname, stats=team_db)
 
 @app.route('/about')
 def about_page():
