9b486884e43c9f76d5f7213297804b0ce2799277
Jack Quimby <jack.quimby54@gmail.com>
2023-10-28 17:10:02 -0400

    fixed performance issue

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from true_odds.routes import load_data
 from true_odds import app, db
 
+update_db = False
 
 if __name__ == '__main__':
-    with app.app_context():
-        #db.create_all()
-        load_data()
+    if update_db:
+        with app.app_context():
+            #db.create_all()
+            load_data()
 
     app.run(debug=True, host='0.0.0.0', port=5000)
