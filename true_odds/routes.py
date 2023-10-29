ce6cf44e90dea13aee8288db9fe29b20267a71c4
Jack Quimby <jack.quimby54@gmail.com>
2023-10-28 22:38:33 -0400

    new page layouts and some new pages!!

--- true_odds/routes.py
+++ true_odds/routes.py
@@ -5,6 +5,8 @@ from flask import redirect, render_template, url_for
 from true_odds.models import Game, Team 
 from sqlalchemy import and_, or_
 from datetime import datetime
+import re
+libraries = {'re': re}
 today_date = datetime.now().date()
 
 paths = {
@@ -23,37 +25,35 @@ def load_data():
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', **libraries)
 
 @app.route('/pred')
 def predict_page():
     data = Game.query.filter(Game.date >= today_date)
-    print(data)
-    return render_template('pred.html', table=data)
+    cols = data.first().__table__.columns.keys()
+    return render_template('pred.html', table=data, columns=cols, **libraries)
 
 @app.route('/team/<teamname>')
 def team_page(teamname):
     team_db = Game.query.filter(or_(Game.A == teamname, Game.B == teamname))
-    return render_template('team_preds.html', name=teamname, table=team_db)
+    stats_db = Team.query.filter(Team.team_name == teamname)
+    cols = stats_db.first().__table__.columns.keys()
+    return render_template('team_preds.html', name=teamname, table=team_db, columns=cols, stats=stats_db, **libraries)
 
 @app.route('/day/<date>')
 def date_page(date):
     date_db = Game.query.filter(Game.date == date)
-    return render_template('team_preds.html', name=date, table=date_db)
+    cols = date_db.first().__table__.columns.keys()
+    return render_template('day_preds.html', day=date, table=date_db, columns=cols, **libraries)
 
-@app.route('/stats/<teamname>')
-def team_stats_page(teamname):
-    team_db = Team.query.filter(Team.team_name == teamname)
-    return render_template('team_bio.html', name=teamname, stats=team_db)
-
-@app.route('/duel/<team_a>_vs_<team_b>')
+@app.route('/duel/<team_a>_vs_<team_b>-<pred_idx>')
 def duel_page(team_a, team_b, pred_idx):
     preds = Game.query.filter(Game.index == pred_idx)
     a_stats = Team.query.filter(Team.team_name == team_a)
     b_stats = Team.query.filter(Team.team_name == team_b)
-    return render_template('duel_page.html', t1=a_stats, t2=b_stats, prediction=preds)
+    return render_template('duel_page.html', t1=a_stats, t2=b_stats, prediction=preds, **libraries)
 
 
 @app.route('/about')
 def about_page():
-    return render_template('about.html')
+    return render_template('about.html', **libraries)
