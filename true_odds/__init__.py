3e05c628f9192120def3518783fd73c2cbe90157
Jack Quimby <jack.quimby54@gmail.com>
2023-11-03 02:53:24 -0400

    index reroute

--- true_odds/__init__.py
+++ true_odds/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///predict.db'
 db = SQLAlchemy(app)
