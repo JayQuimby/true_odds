147b3f2136f606f25eeb71725693fceab7df658c
Jack Quimby <jack.quimby54@gmail.com>
2023-11-03 02:41:53 -0400

    firebase deployment

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ from true_odds.routes import load_data
 from true_odds import app, db
 
 update_db = False
-
+testing = True
 if __name__ == '__main__':
     if update_db:
         with app.app_context():
             db.create_all()
             load_data()
-
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    if testing:
+        app.run(debug=True, host='0.0.0.0', port=5000)
+    else:
+        app.run(port=5000)
